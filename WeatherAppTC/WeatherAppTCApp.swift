1e1c0148f2ea2151152c9146bd5bd5825b797aeb
Přemysl Mikulenka <premysl.mikulenka@icloud.com>
2024-01-17 22:06:09 +0100

    Initial Commit

---
+++ WeatherAppTC/WeatherAppTCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTCApp.swift
+//  WeatherAppTC
+//
+//  Created by Přemysl Mikulenka on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppTCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
