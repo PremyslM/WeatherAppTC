e2ba95f5176040bb6cf77ed5c9ff79b6f99f0946
Přemysl Mikulenka <premysl.mikulenka@icloud.com>
2024-02-04 21:32:30 +0100

    Optimised searchBar

--- WeatherAppTC/Presentation/UI/Common/Components/CustomSearchBarView.swift
+++ WeatherAppTC/Presentation/UI/Common/Components/CustomSearchBarView.swift
@@ -7,38 +7,41 @@
 
 import SwiftUI
 
-struct CustomSearchBarView: View {    
-    @State private var searchText: String = "Czech"
+struct CustomSearchBarView: View {
+    @State private var searchText: String = ""
     @State private var isEditing: Bool = false
-    
+
     let onSearchSubmit: (String) -> Void
-    
+
     var body: some View {
         HStack {
-            TextField("Search ...", text: $searchText)
+            lazy var textField: some View = {
+                TextField("Search ...", text: $searchText) {
+                    onSearchSubmit(searchText)
+                }
                 .padding(7)
                 .padding(.horizontal, 25)
                 .background(Color(.white.opacity(0.5)))
                 .cornerRadius(8)
+            }()
+
+            textField
                 .onTapGesture {
                     self.isEditing = true
                 }
-                .onSubmit {
-                    onSearchSubmit(searchText)
-                }
-            
+
             if isEditing {
                 Button(action: {
                     self.isEditing = false
                     self.searchText = ""
-                    
                 }) {
                     Image(systemName: "xmark.circle.fill")
                         .renderingMode(.original)
+                        .padding(.horizontal, 10)
                 }
-                .padding(.horizontal, 10)
-                .transition(.move(edge: .trailing))
+                //.transition(.move(edge: .trailing))
             }
         }
     }
 }
+
